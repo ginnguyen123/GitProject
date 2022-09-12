34ea88acb2be6b66b599fda4c098aebe117042b6
your name <nhuunghia95@gmail.com>
2022-09-12 02:00:53 -0700

    [Thực hành] Khai báo và sử dụng biến

---
+++ JVS/Variable/UseVar.js
@@ -0,0 +1,12 @@
+let i = 10
+let f = 20.5
+let b = true
+let s = 'Hà Nội'
+
+document.writeln('Giá trị i =' + i)
+document.write('<br/>')
+document.write('Giá trị f = ' + f)
+document.write('<br/>')
+document.write('Giá trị b = ' + b)
+document.write('<br/>')
+document.write('Giá trị s = ' + s)
